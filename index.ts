67cda94fce13c198c5384a80c799ff249c572a33
GUERCHAOUI <MNG@MacBook-Pro-de-GUERCHAOUI.local>
2024-11-13 00:08:45 +0100

    first commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import express, { Response, Request } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+require("dotenv").config();
+
+const connectMongoDB = async () => {
+  try {
+    if (!process.env.MONGODB_URI) {
+      console.log("MONGODB_URI is not defined in the environment variables");
+      return;
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+connectMongoDB();
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({ message: "Note Fond Vinted TS" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server STARTED 📡");
+});
