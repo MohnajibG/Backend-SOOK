070a23898d0f7bdefd151cc5a17fc19164f891e8
GUERCHAOUI <MNG@MacBook-Pro-de-GUERCHAOUI.local>
2024-11-13 01:11:27 +0100

    model user added

--- models/User.ts
+++ models/User.ts
@@ -1,1 +1,26 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+
+type UserProps = {
+  email: string;
+};
+
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  account: {
+    username: {
+      type: String,
+      required: true,
+    },
+    avatar: Object,
+  },
+  newslettre: Boolean,
+  token: String,
+  hash: String,
+  salt: String,
+});
+
+module.exports = User;
