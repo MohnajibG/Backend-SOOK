d9b51994d55a2862f55445800df0e5f4a0451f84
GUERCHAOUI <guerchaoui7@hotmail.com>
2024-12-12 14:42:39 +0100

    fix(offer) route getOffers added

--- src/controllers/offerControler.ts
+++ src/controllers/offerControler.ts
@@ -3,12 +3,12 @@ import cloudinary from "cloudinary";
 import Offer from "../models/Offer";
 import { UploadedFile } from "express-fileupload"; // Type pour le fichier uploadé
 
-// Configuration de Cloudinary
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// // Configuration de Cloudinary
+// cloudinary.v2.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 // Fonction pour publier l'offre avec plusieurs images
 export const publishOffer = async (
@@ -88,6 +88,27 @@ export const publishOffer = async (
     });
   } catch (error) {
     console.error("Erreur lors de la publication de l'offre:", error);
+    res.status(500).json({ message: "Erreur interne du serveur." });
+  }
+};
+export const getOffers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    // Récupérer toutes les offres depuis la base de données
+    const offers = await Offer.find();
+
+    if (!offers || offers.length === 0) {
+      res.status(404).json({ message: "Aucune offre trouvée." });
+      return;
+    }
+
+    // Retourner les offres sous forme de réponse JSON
+    res.status(200).json({ offers });
+  } catch (error) {
+    console.error("Erreur lors de la récupération des offres:", error);
     res.status(500).json({ message: "Erreur interne du serveur." });
   }
 };
--- src/routes/odderRoutes.ts
+++ src/routes/odderRoutes.ts
@@ -1,16 +1,10 @@
 import express from "express";
-import fileUpload from "express-fileupload";
-// import isAuthenticated from "../middlewares/isAuthenticated";
+import { getOffers, publishOffer } from "../controllers/offerControler"; // import isAuthenticated from "../middlewares/isAuthenticated";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
-router.post("/offers/publish", fileUpload(), async (req, res) => {
-  try {
-    // Le contrôleur sera implémenté ici
-    res.status(200).json({ message: "Route accessible." });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
+router.post("/offers/publish", isAuthenticated, publishOffer);
 
+router.get("/offers", getOffers);
 export default router;
