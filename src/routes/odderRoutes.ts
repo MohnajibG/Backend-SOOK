7a48836c785e78fbd03557fb7f84f107dbcec7fe
GUERCHAOUI <guerchaoui7@hotmail.com>
2024-12-12 12:12:04 +0100

    fix( middleware):isAuth

--- src/routes/odderRoutes.ts
+++ src/routes/odderRoutes.ts
@@ -4,18 +4,13 @@ import isAuthenticated from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
-router.post(
-  "/offers/publish",
-  fileUpload(),
-  isAuthenticated,
-  async (req, res) => {
-    try {
-      // Le contrôleur sera implémenté ici
-      res.status(200).json({ message: "Route accessible." });
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
+router.post("/offers/publish", fileUpload(), async (req, res) => {
+  try {
+    // Le contrôleur sera implémenté ici
+    res.status(200).json({ message: "Route accessible." });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-);
+});
 
 export default router;
