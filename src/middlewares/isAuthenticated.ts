1958b3e33bb83e3dafcd296f00de705718bbf5bd
GUERCHAOUI <guerchaoui7@hotmail.com>
2024-12-12 12:20:57 +0100

    fix( middleware):isAuth

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,35 +1,35 @@
-import { Request, Response, NextFunction } from "express";
-import User from "../models/User";
+// import { Request, Response, NextFunction } from "express";
+// import User from "../models/User";
 
-interface AuthenticatedRequest extends Request {
-  user: any;
-}
+// interface AuthenticatedRequest extends Request {
+//   user: any;
+// }
 
-const isAuthenticated = async (
-  req: AuthenticatedRequest,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  try {
-    if (!req.headers.authorization) {
-      res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
+// const isAuthenticated = async (
+//   req: AuthenticatedRequest,
+//   res: Response,
+//   next: NextFunction
+// ): Promise<void> => {
+//   try {
+//     if (!req.headers.authorization) {
+//       res.status(401).json({ message: "Unauthorized" });
+//       return;
+//     }
 
-    const token = req.headers.authorization.replace("Bearer ", "");
-    const user = await User.findOne({ token: token });
+//     const token = req.headers.authorization.replace("Bearer ", "");
+//     const user = await User.findOne({ token: token });
 
-    if (!user) {
-      res.status(401).json({ message: "Unauthorized" });
-      return;
-    }
+//     if (!user) {
+//       res.status(401).json({ message: "Unauthorized" });
+//       return;
+//     }
 
-    req.user = user;
-    return next();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-    return;
-  }
-};
+//     req.user = user;
+//     return next();
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//     return;
+//   }
+// };
 
-export default isAuthenticated;
+// export default isAuthenticated;
