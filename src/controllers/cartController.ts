2cd6a55bc0c14eb2c5b0dbdac2cc09be7aef8420
GUERCHAOUI <guerchaoui7@hotmail.com>
2025-01-05 01:03:11 +0100

    fix(cart) add router

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -30,3 +30,45 @@ export const getCart = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteCart = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await Cart.findOneAndDelete({ id });
+
+    if (result) {
+      res.status(200).json({ message: "Article supprimé du panier" });
+    } else {
+      res.status(404).json({ error: "Article non trouvé dans le panier" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateCart = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { quantity } = req.body;
+
+  if (quantity <= 0) {
+    res.status(400).json({ error: "La quantité doit être supérieure à zéro." });
+  }
+
+  try {
+    // Mettre à jour la quantité de l'article
+    const updatedItem = await Cart.findOneAndUpdate(
+      { id },
+      { $set: { quantity } },
+      { new: true }
+    );
+
+    if (updatedItem) {
+      res.status(200).json({ message: "Quantité mise à jour", updatedItem });
+    } else {
+      res.status(404).json({ error: "Article non trouvé dans le panier" });
+    }
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour de l'article :", error);
+    res.status(500).json({ error: "Erreur serveur lors de la mise à jour" });
+  }
+};
--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import isAuthenticated from "../middlewares/isAuthenticated";
 
-import { getCart, addCart } from "../controllers/cartController";
+import {
+  getCart,
+  addCart,
+  deleteCart,
+  updateCart,
+} from "../controllers/cartController";
 
 const router = Router();
 
 router.get("/cart", isAuthenticated, getCart);
 router.post("/cart/add", isAuthenticated, addCart);
+router.delete("/cart/delete/:id", isAuthenticated, deleteCart);
+router.put("/cart/update/:id", isAuthenticated, updateCart);
 
 export default router;
