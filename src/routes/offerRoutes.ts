420de00ba687d98a1d3aa9fe9f8051ecf32eca5c
GUERCHAOUI <guerchaoui7@hotmail.com>
2025-08-29 17:00:36 +0200

    fix offerController

--- src/routes/offerRoutes.ts
+++ src/routes/offerRoutes.ts
@@ -16,12 +16,15 @@ const router = express.Router();
 // Routes publiques
 router.get("/offers", getOffers);
 router.get("/offers/search", searchOffers);
+
+// ⚠️ ordre important
+router.get("/offers/user", isAuthenticated, getMyOffers); // mes annonces (auth)
 router.get("/offers/user/:userId", getOfferByUserId);
+
 router.get("/offers/:id", getOfferById);
 
 // Routes protégées
 router.post("/offers/publish", isAuthenticated, publishOffer);
-router.get("/offers/user", isAuthenticated, getMyOffers); // ✅ mes annonces (auth)
 router.put("/offers/update/:id", isAuthenticated, updateOffer);
 router.delete("/offers/delete/:id", isAuthenticated, deleteOffer);
 
